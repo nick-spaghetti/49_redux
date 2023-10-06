465ce3f0e1515992f78599a4716d839cb933a1cd
nick <nick.salvetti@live.com>
2023-10-05 19:27:02 -0500

    100523 19:27

--- 49.4/microblog/src/actions/creator.js
+++ 49.4/microblog/src/actions/creator.js
@@ -1,1 +1,18 @@
 import axios from "axios";
+import { FETCH_POST } from "./types";
+
+const API_URL = process.env.DATABASE_URL || "http://localhost:5000/api/posts";
+
+export const getPostFromAPI = (id) => {
+	return async function (dispatch) {
+		const res = await axios.get(`${API_URL}/${id}`);
+		return dispatch(getPost(res.data));
+	};
+};
+
+const getPost = (post) => {
+	return {
+		type: FETCH_POST,
+		post,
+	};
+};
---
+++ 49.4/microblog/src/actions/types.js
@@ -0,0 +1,8 @@
+export const FETCH_TITLES = "FETCH_TITLES";
+export const FETCH_POST = "FETCH_POST";
+export const ADD_POST = "ADD_POST";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const UPDATE_POST = "UPDATE_POST";
+export const REMOVE_POST = "REMOVE_POST";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+export const VOTE = "VOTE";
